6e817b7cd00c3ae2a28db427f7b17a6a03ae4a3a
Alexdevid <kerdevid@gmail.com>
2015-01-27 20:22:10 +0300

    more cors!

--- app/Components/Rest.php
+++ app/Components/Rest.php
@@ -46,10 +46,6 @@ class Rest
 
     private function response()
     {
-        //header("HTTP/1.1 $code $status");
-        header('Content-type: application/json');
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, PUT');
         echo $this->response;
     }
 
