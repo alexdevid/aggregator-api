123a947a6362c11f45d9f759636fb9ca9e4588a5
Alexdevid <kerdevid@gmail.com>
2015-01-27 20:17:13 +0300

    bla

--- app/Components/Rest.php
+++ app/Components/Rest.php
@@ -46,7 +46,7 @@ class Rest
 
     private function response()
     {
-        header("HTTP/1.1 $code $status");
+        //header("HTTP/1.1 $code $status");
         header('Content-type: application/json');
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, PUT');
