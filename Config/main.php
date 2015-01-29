bb2444759b375821c0ce49636cf50b5c2843fe89
Alexander <kerdevid@gmail.com>
2015-01-29 09:52:11 +0300

    clear master

--- Config/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	'db' => [
-		'username' => 'root',
-		'password' => '',
-		'dbname' => 'aggr'
-	]
-];
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-$config = require_once __DIR__ . DIRECTORY_SEPARATOR . 'Config/main.php';
-spl_autoload_extensions(".php");
-spl_autoload_register();
-
-$app = new Application\App($config);
