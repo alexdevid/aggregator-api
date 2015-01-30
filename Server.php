6a45314ed4079b93d18ac09eb8784c7e5022154d
Alexander <kerdevid@gmail.com>
2015-01-30 09:49:57 +0300

    autoload PSR-4

--- Server.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . '/lib/Request.php';
-require_once __DIR__ . '/lib/Response.php';
-require_once __DIR__ . '/lib/RestController.php';
-require_once __DIR__ . '/lib/RestServer.php';
